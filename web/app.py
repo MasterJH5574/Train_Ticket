14f95bb0200a2304201266407fb092f7f4ae995b
MasterJH5574 <45167100+MasterJH5574@users.noreply.github.com>
2019-05-01 23:56:59 +0800

    mainpage/login/register first with jinja

--- web/app.py
+++ web/app.py
@@ -4,22 +4,39 @@ import client
 from flask import Flask, render_template, session, redirect, url_for, request
 
 app = Flask(__name__)
-# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+def get_privilege(user_id):
+    return 1
+
 
 @app.route("/")
 def initial():
-    return render_template("main_page.html")
-    # return "已登录 ? 主页 : 登录页"
+    current_user = session.get('user_id')
+    app.logger.debug(current_user)
+    if current_user:
+        return render_template("main_page.html",
+                               user_id=current_user,
+                               administrator=get_privilege(current_user)
+                               )
+    else:
+        return render_template("main_page.html",
+                               user_id=current_user  # == None
+                               )
 
 
 @app.route("/login")
 def login():
     current_user = session.get('user_id')
+    app.logger.debug(current_user)
     if current_user:
         # alart 已登录
         return 1 #TODO alart & return to main page
     else:
-        return render_template("login.html")
+        return render_template("login.html",
+                               user_id=current_user
+                               )
 
 
 @app.route("/user_login", methods=['POST'])  # method=['GET', 'POST']
@@ -44,6 +61,18 @@ def register():
     current_user = session.get('user_id')
     if current_user:
         # alart 已登录
-        return 1 #TODO alart & return to main page
+        return 1  #TODO alart & return to main page
     else:
-        return render_template("register.html")
+        return render_template("register.html",
+                               user_id=current_user
+                               )
+
+
+@app.route("/user_register", methods=['POST'])
+def user_register():
+    return 0
+
+
+if __name__ == '__main__':
+    # app.config.from_object('config')
+    app.run(host="127.0.0.1", port=5000, debug=True)
