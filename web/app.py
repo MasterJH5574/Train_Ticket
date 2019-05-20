edb889660d84c787d40986b53d88bbec6f012fd3
MasterJH5574 <45167100+MasterJH5574@users.noreply.github.com>
2019-05-21 07:51:58 +0800

    deal with tickets

--- web/app.py
+++ web/app.py
@@ -1,7 +1,7 @@
 # filename app.py
 
 import client
-from flask import Flask, render_template, session, redirect, url_for, request, flash
+from flask import Flask, render_template, session, redirect, url_for, request, flash, jsonify
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -10,6 +10,7 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.route("/")
 def main_page():
     return render_template("main_page.html",
+                           user_id=session.get('user_id'),
                            username=session.get('username'),
                            privilege=session.get('privilege')
                            )
@@ -23,7 +24,11 @@ def login():
             flash("info 请勿重复登录！ 您已登录，请勿重复登录")
             return redirect(url_for('main_page'))
         else:
-            return redirect(url_for('login'))
+            return render_template("login.html",
+                                   user_id=None,
+                                   username=None,
+                                   privilege=None
+                                   )
 
     else:
         element = ('user_id', 'password')
@@ -55,6 +60,7 @@ def register():
             return redirect(url_for("main_page"))
         else:
             return render_template("register.html",
+                                   user_id=None,
                                    username=None,
                                    privilege=None
                                    )
@@ -148,6 +154,7 @@ def modify_profile():
             return redirect(url_for("login"))
         else:
             return render_template("modify_profile.html",
+                                   user_id=session.get('user_id'),
                                    username=session.get('username'),
                                    privilege=session.get('privilege')
                                    )
@@ -199,6 +206,7 @@ def modify_privilege():
 
     if request.method == 'GET':
         return render_template("modify_privilege.html",
+                               user_id=session.get('user_id'),
                                username=session.get('username'),
                                privilege=session.get('privilege')
                                )
@@ -210,6 +218,206 @@ def modify_privilege():
     else:
         flash("success 操作成功！ 已成功将用户{}升级为管理员".format(user_id))
     return redirect(url_for('modify_privilege'))
+
+
+@app.route('/tickets', methods=['POST', 'GET'])
+def query_ticket():
+    if request.method == 'GET':
+        return render_template("query_ticket.html",
+                               user_id=session.get('user_id'),
+                               username=session.get('username'),
+                               privilege=session.get('privilege')
+                               )
+    else:
+        user_id = session.get('user_id')
+        if not user_id:
+            flash("danger 尚未登录！ 请登录后再进行操作")
+            return redirect(url_for('login'))
+        train_id = request.form['train_id']
+        source = request.form['source']
+        terminal = request.form['terminal']
+        time_departure = request.form['time_departure']
+        seat_list = request.form['seat'].split(' ')
+        seat = seat_list[0]
+        price = seat_list[1]
+        return render_template("buy_confirm.html",
+                               user_id=session.get('user_id'),
+                               username=session.get('username'),
+                               privilege=session.get('privilege'),
+                               train_id=train_id,
+                               source=source,
+                               terminal=terminal,
+                               seat=seat,
+                               time_departure=time_departure,
+                               price=price
+                               )
+
+
+@app.route('/buy_ticket', methods=['POST', 'GET'])
+def buy_ticket():
+    if request.method == 'GET':
+        flash("danger 访问失败！ 请先选择车票")
+        return redirect(url_for('main_page'))
+
+    user_id = session.get('user_id')
+    if not user_id:
+        flash("danger 尚未登录！ 请登录后再进行操作")
+        return redirect(url_for('login'))
+
+    train_id = request.form['train_id']
+    source = request.form['source']
+    terminal = request.form['terminal']
+    date = request.form['date']
+    seat = request.form['seat'].split(' ')[0]
+
+    res = client.buy_ticket(user_id, train_id, source, terminal, date, seat)
+    if res == 1:
+        flash("success 购票成功！ 您已成功购票！")
+    else:
+        flash("danger 购票失败！ 未知错误，请联系管理员或再次尝试操作")
+    return redirect(url_for('main_page'))
+
+
+@app.route('/tickets/query_general_ajax')
+def query_general_ajax():
+    s = request.args.get("S")
+    t = request.args.get("T")
+    date = request.args.get("date")
+    type_c = request.args.get("type_C") == "true"
+    type_d = request.args.get("type_D") == "true"
+    type_g = request.args.get("type_G") == "true"
+    type_k = request.args.get("type_K") == "true"
+    type_o = request.args.get("type_O") == "true"
+    type_t = request.args.get("type_T") == "true"
+    type_z = request.args.get("type_Z") == "true"
+
+    res = ""
+    if type_c:
+        res += client.query_ticket(s, t, date, "C")
+    if type_d:
+        res += client.query_ticket(s, t, date, "D")
+    if type_g:
+        res += client.query_ticket(s, t, date, "G")
+    if type_k:
+        res += client.query_ticket(s, t, date, "K")
+    if type_o:
+        res += client.query_ticket(s, t, date, "O")
+    if type_t:
+        res += client.query_ticket(s, t, date, "T")
+    if type_z:
+        res += client.query_ticket(s, t, date, "Z")
+    return res
+
+
+@app.route('/tickets/query_transit_ajax')
+def query_transit_ajax():
+    s = request.args.get("S")
+    t = request.args.get("T")
+    date = request.args.get("date")
+    type_c = request.args.get("type_C") == "true"
+    type_d = request.args.get("type_D") == "true"
+    type_g = request.args.get("type_G") == "true"
+    type_k = request.args.get("type_K") == "true"
+    type_o = request.args.get("type_O") == "true"
+    type_t = request.args.get("type_T") == "true"
+    type_z = request.args.get("type_Z") == "true"
+
+    res = ""
+    if type_c:
+        res += client.query_transfer(s, t, date, "C")
+    if type_d:
+        res += client.query_transfer(s, t, date, "D")
+    if type_g:
+        res += client.query_transfer(s, t, date, "G")
+    if type_k:
+        res += client.query_transfer(s, t, date, "K")
+    if type_o:
+        res += client.query_transfer(s, t, date, "O")
+    if type_t:
+        res += client.query_transfer(s, t, date, "T")
+    if type_z:
+        res += client.query_transfer(s, t, date, "Z")
+    return res
+
+
+@app.route('/my_ticket', methods=["GET", "POST"])
+def my_ticket():
+    user_id = session.get("user_id")
+    if not user_id:
+        flash("danger 尚未登录！ 请登录后再进行操作")
+        return redirect(url_for('login'))
+
+    if request.method == "GET":
+        return render_template("my_ticket.html",
+                               user_id=session.get('user_id'),
+                               username=session.get('username'),
+                               privilege=session.get('privilege')
+                               )
+
+    train_id = request.form['train_id']
+    source = request.form['source']
+    terminal = request.form['terminal']
+    date = request.form['time'].split(" ")[0]
+    seat = request.form['seat']
+
+    res = client.refund_ticket(user_id, train_id, source, terminal, date, seat)
+    if res == 1:
+        flash("success 退票成功！ 您已成功退票")
+    else:
+        flash("danger 退票失败！ 未知错误")
+    return redirect(url_for("main_page"))
+
+
+@app.route('/my_ticket/ajax')
+def my_ticket_ajax():
+    user_id = session.get("user_id")
+    if not user_id:
+        flash("danger 尚未登录！ 请登录后再进行操作")
+        return redirect(url_for('login'))
+
+    date = request.args.get("date")
+    type_c = request.args.get("type_C") == "true"
+    type_d = request.args.get("type_D") == "true"
+    type_g = request.args.get("type_G") == "true"
+    type_k = request.args.get("type_K") == "true"
+    type_o = request.args.get("type_O") == "true"
+    type_t = request.args.get("type_T") == "true"
+    type_z = request.args.get("type_Z") == "true"
+
+    res = ""
+    if date == "":
+        if type_c:
+            res += client.query_order_all(user_id, "C")
+        if type_d:
+            res += client.query_order_all(user_id, "D")
+        if type_g:
+            res += client.query_order_all(user_id, "G")
+        if type_k:
+            res += client.query_order_all(user_id, "K")
+        if type_o:
+            res += client.query_order_all(user_id, "O")
+        if type_t:
+            res += client.query_order_all(user_id, "T")
+        if type_z:
+            res += client.query_order_all(user_id, "Z")
+    else:
+        if type_c:
+            res += client.query_order(user_id, date, "C")
+        if type_d:
+            res += client.query_order(user_id, date, "D")
+        if type_g:
+            res += client.query_order(user_id, date, "G")
+        if type_k:
+            res += client.query_order(user_id, date, "K")
+        if type_o:
+            res += client.query_order(user_id, date, "O")
+        if type_t:
+            res += client.query_order(user_id, date, "T")
+        if type_z:
+            res += client.query_order(user_id, date, "Z")
+            
+    app.logger.debug(res)
+    return res
 
 
 if __name__ == '__main__':
--- web/client.py
+++ web/client.py
@@ -10,7 +10,7 @@ def send(message):
     global s
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(60)
+        s.settimeout(600)
         s.connect((host, port))
         s.send(message.encode('utf-8'))
 
@@ -55,3 +55,226 @@ def get_privilege(user_id):
 
 def modify_privilege(id1, id2, privilege):
     return int(send("modify_privilege {} {} {}".format(id1, id2, privilege)))
+
+
+def query_ticket(source, terminal, date, catalog):
+    res = send("query_ticket_frontend {} {} {} {}".format(source, terminal, date, catalog)).split(" ")
+    tot_line = int(res[0])
+    if tot_line == -1:
+        table = '<tr><td colspan="15"><p class="h5">查询非法，请重新查询</p></td></tr>'
+        return table
+    if tot_line == 0:
+        table = '<tr><td colspan="15"><p class="h5">没有找到符合要求的车次</p></td></tr>'
+        return table
+
+    index = 1
+    table = ""
+    for i in range(0, tot_line):
+        seat_cnt = 0
+
+        table += "<tr><td>" + res[index] + "</td>"
+        table += "<td>" + res[index + 1] + "<br>" + res[index + 4] + "</td>"
+        table += "<td>" + res[index + 2] + " " + res[index + 3] + \
+                 "<br>" + res[index + 5] + " " + res[index + 6] + "</td>"
+
+        ticket_type = {"商务座": (0, 0),
+                       "特等座": (0, 0),
+                       "一等座": (0, 0),
+                       "二等座": (0, 0),
+                       "高级软卧": (0, 0),
+                       "软卧": (0, 0),
+                       "动卧": (0, 0),
+                       "硬卧": (0, 0),
+                       "软座": (0, 0),
+                       "硬座": (0, 0),
+                       "无座": (0, 0)
+                       }
+
+        item_cnt = int(res[index + 7])
+        for j in range(0, item_cnt):
+            typename = res[index + 7 + 3 * j + 1]
+            if typename not in ticket_type.keys():
+                continue
+            cnt = int(res[index + 7 + 3 * j + 2])
+            seat_cnt += cnt
+            price = float(res[index + 7 + 3 * j + 3])
+            ticket_type[typename] = (cnt, price)
+
+        for value in ticket_type.values():
+            if value[0] == 0:
+                table += "<td>无</td>"
+            else:
+                table += "<td>" + str(value[0]) + "<br>" + str(value[1]) + "元</td>"
+        if seat_cnt != 0:
+            table += '<td><button class="btn btn-primary btn-buy" '
+            table += 'data-toggle="modal" data-target="#buy_modal">购买</button></td></tr>'
+        else:
+            table += '<td><button class="btn btn-secondary" disabled>购买</button></td></tr>'
+        index += 8 + 3 * item_cnt
+
+    return table
+
+
+def query_transfer(source, terminal, date, catalog):
+    res = send("query_transfer_frontend {} {} {} {}".format(source, terminal, date, catalog)).split(" ")
+    if res[0] == "-1":
+        table = '<tr><td colspan="15"><p class="h5">查询非法，或没有找到符合要求的车次</p></td></tr>'
+        return table
+
+    index = 0
+    table = ""
+    for i in range(0, 2):
+        seat_cnt = 0
+
+        table += "<tr><td>" + res[index] + "</td>"
+        table += "<td>" + res[index + 1] + "<br>" + res[index + 4] + "</td>"
+        table += "<td>" + res[index + 2] + " " + res[index + 3] + \
+                 "<br>" + res[index + 5] + " " + res[index + 6] + "</td>"
+
+        ticket_type = {"商务座": (0, 0),
+                       "特等座": (0, 0),
+                       "一等座": (0, 0),
+                       "二等座": (0, 0),
+                       "高级软卧": (0, 0),
+                       "软卧": (0, 0),
+                       "动卧": (0, 0),
+                       "硬卧": (0, 0),
+                       "软座": (0, 0),
+                       "硬座": (0, 0),
+                       "无座": (0, 0)
+                       }
+
+        item_cnt = int(res[index + 7])
+        for j in range(0, item_cnt):
+            typename = res[index + 7 + 3 * j + 1]
+            if typename not in ticket_type.keys():
+                continue
+            cnt = int(res[index + 7 + 3 * j + 2])
+            seat_cnt += cnt
+            price = float(res[index + 7 + 3 * j + 3])
+            ticket_type[typename] = (cnt, price)
+
+        for value in ticket_type.values():
+            if value[0] == 0:
+                table += "<td>无</td>"
+            else:
+                table += "<td>" + str(value[0]) + "<br>" + str(value[1]) + "元</td>"
+        if seat_cnt != 0:
+            table += '<td><button class="btn btn-primary btn-buy" '
+            table += 'data-toggle="modal" data-target="#buy_modal">购买</button></td></tr>'
+        else:
+            table += '<td><button class="btn btn-secondary" disabled>购买</button></td></tr>'
+        index += 8 + 3 * item_cnt
+
+    return table
+
+
+def buy_ticket(user_id, train_id, source, terminal, date, seat):
+    return int(send("buy_ticket {} {} {} {} {} {} {}".format(user_id, str(1), train_id,
+                                                             source, terminal, date, seat)))
+
+
+def query_order(user_id, date, catalog):
+    res = send("query_order_frontend {} {} {}".format(user_id, date, catalog)).split(' ')
+    tot_line = int(res[0])
+    if tot_line == -1:
+        table = '<tr><td colspan="6"><p class="h5">查询非法，您没有购买车票</p></td></tr>'
+        return table
+
+    if tot_line == 0:
+        table = '<tr><td colspan="6"><p class="h5">您没有购买车票</p></td></tr>'
+        return table
+
+    index = 1
+    table = ""
+    for i in range(0, tot_line):
+        ticket_type = {"商务座": 0,
+                       "特等座": 0,
+                       "一等座": 0,
+                       "二等座": 0,
+                       "高级软卧": 0,
+                       "软卧": 0,
+                       "动卧": 0,
+                       "硬卧": 0,
+                       "软座": 0,
+                       "硬座": 0,
+                       "无座": 0
+                       }
+
+        item_cnt = int(res[index + 7])
+        for j in range(0, item_cnt):
+            typename = res[index + 7 + 3 * j + 1]
+            if typename not in ticket_type.keys():
+                continue
+            cnt = int(res[index + 7 + 3 * j + 2])
+            ticket_type[typename] = cnt
+
+        for key, value in ticket_type.items():
+            if value != 0:
+                table += "<tr><td>" + res[index] + "</td>"
+                table += "<td>" + res[index + 1] + "<br>" + res[index + 4] + "</td>"
+                table += "<td>" + res[index + 2] + " " + res[index + 3] + \
+                         "<br>" + res[index + 5] + " " + res[index + 6] + "</td>"
+                table += "<td>" + key + "</td>"
+                table += "<td>" + str(value) + "</td>"
+                table += '<td><button class="btn btn-primary btn-refund" '
+                table += 'data-toggle="modal" data-target="#refund_modal">退票</button></td></tr>'
+
+        index += 8 + 3 * item_cnt
+
+    return table
+
+
+def query_order_all(user_id, catalog):
+    res = send("query_order_all {} {}".format(user_id, catalog)).split(" ")
+    tot_line = int(res[0])
+    if tot_line == -1:
+        table = '<tr><td colspan="6"><p class="h5">查询非法，您没有购买车票</p></td></tr>'
+        return table
+
+    if tot_line == 0:
+        table = '<tr><td colspan="6"><p class="h5">您没有购买车票</p></td></tr>'
+        return table
+
+    index = 1
+    table = ""
+    for i in range(0, tot_line):
+        ticket_type = {"商务座": 0,
+                       "特等座": 0,
+                       "一等座": 0,
+                       "二等座": 0,
+                       "高级软卧": 0,
+                       "软卧": 0,
+                       "动卧": 0,
+                       "硬卧": 0,
+                       "软座": 0,
+                       "硬座": 0,
+                       "无座": 0
+                       }
+
+        item_cnt = int(res[index + 7])
+        for j in range(0, item_cnt):
+            typename = res[index + 7 + 3 * j + 1]
+            if typename not in ticket_type.keys():
+                continue
+            cnt = int(res[index + 7 + 3 * j + 2])
+            ticket_type[typename] = cnt
+
+        for key, value in ticket_type.items():
+            if value != 0:
+                table += "<tr><td>" + res[index] + "</td>"
+                table += "<td>" + res[index + 1] + "<br>" + res[index + 4] + "</td>"
+                table += "<td>" + res[index + 2] + " " + res[index + 3] + \
+                         "<br>" + res[index + 5] + " " + res[index + 6] + "</td>"
+                table += "<td>" + key + "</td>"
+                table += "<td>" + str(value) + "</td>"
+                table += '<td><button class="btn btn-primary btn-refund" '
+                table += 'data-toggle="modal" data-target="#refund_modal">退票</button></td></tr>'
+
+        index += 8 + 3 * item_cnt
+
+    return table
+
+
+def refund_ticket(user_id, train_id, source, terminal, date, seat):
+    return int(send("refund_ticket {} {} {} {} {} {}".format(user_id, str(1), train_id, source, terminal, date, seat)))
